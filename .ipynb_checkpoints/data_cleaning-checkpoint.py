07bc4b66fcf1a10c54123ee2d092540d8ac81f80
Martyna <martyna.durko@gmail.com>
2022-06-17 14:30:05 +0200

    Added data cleaning notebooks and obtained pkl files

---
+++ .ipynb_checkpoints/data_cleaning-checkpoint.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 09:56:42 2022
+
+@author: Martyna
+"""
+
+import pandas as pd
+
+df_2019 = pd.read_csv("datasets/netflix_titles_2019.csv")
+df_2020 = pd.read_csv("datasets/netflix_titles_2020.csv")
+df_2021 = pd.read_csv("datasets/netflix_titles_2021.csv")
