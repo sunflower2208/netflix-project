c6531ca5a39b5736313495e510c8274801d852af
Martyna <martyna.durko@gmail.com>
2022-07-14 16:11:35 +0200

    update of file

--- rating_model/input_dataset_creation.py
+++ rating_model/input_dataset_creation.py
@@ -7,21 +7,35 @@ Created on Mon Jun 27 13:48:40 2022
 
 import pandas as pd
 
-file_1 = pd.read_csv("netflix-prize\combined_data_1.txt", sep=",", header=None, names=["userID", "rating", "ratingDate"])
-file_2 = pd.read_csv("netflix-prize\combined_data_2.txt", sep=",", header=None, names=["userID", "rating", "ratingDate"])
-file_3 = pd.read_csv("netflix-prize\combined_data_3.txt", sep=",", header=None, names=["userID", "rating", "ratingDate"])
-file_4 = pd.read_csv("netflix-prize\combined_data_4.txt", sep=",", header=None, names=["userID", "rating", "ratingDate"])
+file_1 = pd.read_csv("netflix-prize\combined_data_1.txt",
+                     sep=",",
+                     header=None,
+                     names=["userID", "rating", "ratingDate"])
+file_2 = pd.read_csv("netflix-prize\combined_data_2.txt",
+                     sep=",",
+                     header=None,
+                     names=["userID", "rating", "ratingDate"])
+file_3 = pd.read_csv("netflix-prize\combined_data_3.txt",
+                     sep=",",
+                     header=None,
+                     names=["userID", "rating", "ratingDate"])
+file_4 = pd.read_csv("netflix-prize\combined_data_4.txt",
+                     sep=",",
+                     header=None,
+                     names=["userID", "rating", "ratingDate"])
 
-file = pd.concat( [file_1, file_2, file_3, file_4],ignore_index=True)
+file = pd.concat([file_1, file_2, file_3, file_4], ignore_index=True)
 
 file_ix = file[file['userID'].str.contains(":")].index
 
 for i in range(len(file_ix)):
-    if i != len(file_ix)-1:
-        file.loc[file_ix[i]+1 : file_ix[i+1]-1,'movieID'] = file.loc[file_ix[i],"userID"][:-1]
+    if i != len(file_ix) - 1:
+        file.loc[file_ix[i] + 1:file_ix[i + 1] - 1,
+                 'movieID'] = file.loc[file_ix[i], "userID"][:-1]
     else:
-        file.loc[file_ix[i]+1 :,'movieID'] = file.loc[file_ix[i],"userID"][:-1]
-        
+        file.loc[file_ix[i] + 1:, 'movieID'] = file.loc[file_ix[i],
+                                                        "userID"][:-1]
+
 df = file.drop(file_ix)
 
 df.reset_index(inplace=True)
--- rating_model/model_svd.py
+++ rating_model/model_svd.py
@@ -10,19 +10,21 @@ df = pd.read_pickle("input_data.pkl")
 
 df = df.sample(n=250000)
 df.reset_index(inplace=True)
-df.drop(['index', 'genres', 'averageRating', 'keywords', 'title'], inplace = True, axis = 1)
+df.drop(['index', 'genres', 'averageRating', 'keywords', 'title'],
+        inplace=True,
+        axis=1)
 
 ### Model with the Surprise library
 
 from surprise import Reader, Dataset, SVD
 from surprise.model_selection import cross_validate, GridSearchCV
 
-reader = Reader(rating_scale=(1,5))
+reader = Reader(rating_scale=(1, 5))
 data = Dataset.load_from_df(df[['userID', 'movieID', 'rating']], reader=reader)
 
 svd = SVD()
 
-cross_validate(svd, data, measures=['RMSE', 'MAE'], cv = 5)
+cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5)
 
 ## Output of the cross validation:
 # {'test_rmse': array([1.02193482, 1.01939073, 1.020927  , 1.0240076 , 1.0192208 ]),
@@ -38,28 +40,30 @@ cross_validate(svd, data, measures=['RMSE', 'MAE'], cv = 5)
 #   0.5706648826599121,
 #   0.45357179641723633)}
 
-
 ## Tuning of the algorithm parameters
 
-param_grid = {'n_epochs': [10, 20, 30], 'lr_all': [0.002, 0.005, 0.01],
-              'reg_all': [0.2, 0.4, 0.6]}
+param_grid = {
+    'n_epochs': [10, 20, 30],
+    'lr_all': [0.002, 0.005, 0.01],
+    'reg_all': [0.2, 0.4, 0.6]
+}
 
-gs = GridSearchCV(SVD, param_grid = param_grid, measures=['RMSE', 'MAE'], cv = 5)
+gs = GridSearchCV(SVD, param_grid=param_grid, measures=['RMSE', 'MAE'], cv=5)
 
 gs.fit(data)
 
 gs.best_score['rmse']
 ## Output: 1.015134097599549
-    
+
 gs.best_score['mae']
 ## Output: 0.8168001041250028
-    
+
 gs.best_params['rmse']
 ## Output: {'n_epochs': 20, 'lr_all': 0.01, 'reg_all': 0.2}
-results_df = pd.DataFrame.from_dict(gs.cv_results)   
+results_df = pd.DataFrame.from_dict(gs.cv_results)
 
-svd = SVD(n_epochs=20, lr_all = 0.01, reg_all = 0.2)
-cross_validate(svd, data, measures=['RMSE', 'MAE'], cv = 5)
+svd = SVD(n_epochs=20, lr_all=0.01, reg_all=0.2)
+cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5)
 ## Output:
 # {'test_rmse': array([1.01319716, 1.01574379, 1.01349017, 1.02069129, 1.01361071]),
 #  'test_mae': array([0.81703475, 0.8175689 , 0.81613889, 0.82151067, 0.81608986]),
@@ -72,4 +76,4 @@ cross_validate(svd, data, measures=['RMSE', 'MAE'], cv = 5)
 #   0.24999117851257324,
 #   0.2501943111419678,
 #   0.406048059463501,
-#   0.5034983158111572)} 
+#   0.5034983158111572)}
